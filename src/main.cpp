510f317d1b70afde3d505d871ca9daa7004b49ce
to-miz <tolga.mizrak@googlemail.com>
2019-04-24 08:46:05 +0200

    Added TODO comments.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@ TODO:
     - Implement multiple output files (header/implementation).
         - Add file io functions, for instance for generating boilerplate code if a file doesn't exist.
     - Implement direct clang-format integration through a command line option.
+    - Implement multiple output files (header/implementation).
+        - Add file io functions, for instance for generating boilerplate code if a file doesn't exist.
+    - Implement direct clang-format integration through a command line option.
+    - Implement tuples/structs.
 FIXME:
     - An expression like range(1, -) results in assertion failure in monotonic_allocator.h.
     - eval_expression errors do not result in returing -1 from main.
